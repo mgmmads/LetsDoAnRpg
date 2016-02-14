c59fe372951e58bed128ec938861452d0e969aa8
mgmmads <andersenmads150693@hotmail.com>
2016-02-14 21:11:44 +0100

    Start
    Created LivingCreature interface and Player.cs, filled player.cs with
    properties needed + LivingCreatures interface. Didn't finish Player.cs
    constructor, but almost got it down. Also added List of items, as a
    comment in the Player.cs for later reference.

---
+++ LetsDoAnRpg/LetsDoAnRpg/LivingBeing.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsDoAnRpg
+{
+    interface LivingBeing
+    {
+        double MaxHealth { get; set;}
+        double CurrentHealth { get; set; }
+        double MaxMana { get; set; }
+        double CurrentMana { get; set; }
+        double Damage { get; set; }
+    }
+}
---
+++ LetsDoAnRpg/LetsDoAnRpg/Player.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsDoAnRpg
+{
+    class Player : LivingBeing
+    {
+        public double MaxHealth { get; set; }
+        public double CurrentHealth { get; set; }
+        public double MaxMana { get; set; }
+        public double CurrentMana { get; set; }
+        public double Damage { get; set; }
+        public int SkillPoints { get; set; }
+        public double ExperienceForNextLevel { get; set; }
+        public double CurrentExperience { get; set; }
+        public int StrengthPoints { get; set; }
+        public int AgilityPoints { get; set; }
+        public int IntelligentPoints { get; set; }
+        //public List<Item> backPack = new List<Item>();
+
+        public Player(int strengthPoints, int agilityPoints, int intelligentPoints)
+        {
+            MaxHealth = 25 + (strengthPoints * 2);
+            CurrentHealth = MaxHealth;
+            MaxMana = 10 + (intelligentPoints * 2);
+            CurrentMana = MaxMana;
+            Damage = 5 + (strengthPoints * 0.5 + agilityPoints * 0.25);
+
+        }
+
+    }
+}
